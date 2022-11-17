22a53c20ac8579bbe0fdfeb6310480788c1b550d
Aline Moura <alinesouzamoura_@hotmail.com>
2022-11-17 10:47:12 -0300

    setup

---
+++ src/componentes/App.js
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <h1>oi</h1>
+    // <div>
+    //     <Topo/>
+    //     <div class="row">
+    //         <Menu />
+    //         <Conteudo />
+    //     </div>
+    // </div>
+  );
+}
